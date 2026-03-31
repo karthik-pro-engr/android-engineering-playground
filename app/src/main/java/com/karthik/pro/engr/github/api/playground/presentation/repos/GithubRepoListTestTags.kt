9cdf634270580518131116b61f58f607fd127aab
karthik-pro-engr <101930095+karthik-pro-engr@users.noreply.github.com>
2026-03-31 23:03:40 +0530

    test(repos-ui): add constants testing tags for compose ui

---
+++ app/src/main/java/com/karthik/pro/engr/github/api/playground/presentation/repos/GithubRepoListTestTags.kt
@@ -0,0 +1,8 @@
+package com.karthik.pro.engr.github.api.playground.presentation.repos
+
+object GithubRepoListTestTags {
+    const val SEARCH_INPUT = "search_input"
+    const val SEARCH_BUTTON = "search_button"
+
+    const val FULL_SCREEN_LOADER = "full_screen_loader"
+}
