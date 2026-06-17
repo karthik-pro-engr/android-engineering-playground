95c83c81df77e0b172b5ad452132fe1065477745
karthik-pro-engr <101930095+karthik-pro-engr@users.noreply.github.com>
2026-06-18 00:20:46 +0530

    feat(database): add RemoteKeysEntity and RemoteKeysDao for paging state management

--- app/src/main/java/com/karthik/pro/engr/github/api/playground/di/DatabaseModule.kt
+++ app/src/main/java/com/karthik/pro/engr/github/api/playground/di/DatabaseModule.kt
@@ -2,6 +2,7 @@ package com.karthik.pro.engr.github.api.playground.di
 
 import android.content.Context
 import androidx.room.Room
+import com.karthik.pro.engr.github.api.data.local.dao.RemoteKeysDao
 import com.karthik.pro.engr.github.api.data.local.dao.RepoDao
 import com.karthik.pro.engr.github.api.data.local.database.DatabaseConstants.DATABASE_NAME
 import com.karthik.pro.engr.github.api.data.local.database.GithubDatabase
@@ -35,4 +36,14 @@ object DatabaseModule {
     ): RepoDao {
         return database.repoDao()
     }
+
+    @Provides
+    @Singleton
+    fun provideRemoteKeysDao(
+        database: GithubDatabase
+    ): RemoteKeysDao {
+        return database.remoteKeysDao()
+    }
+
+
 }
--- data/src/main/java/com/karthik/pro/engr/github/api/data/local/database/GithubDatabase.kt
+++ data/src/main/java/com/karthik/pro/engr/github/api/data/local/database/GithubDatabase.kt
@@ -4,19 +4,24 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.karthik.pro.engr.github.api.data.local.converter.ListStringConverter
+import com.karthik.pro.engr.github.api.data.local.dao.RemoteKeysDao
 import com.karthik.pro.engr.github.api.data.local.dao.RepoDao
+import com.karthik.pro.engr.github.api.data.local.entity.RemoteKeysEntity
 import com.karthik.pro.engr.github.api.data.local.entity.RepoEntity
 import com.karthik.pro.engr.github.api.data.local.entity.RepoSearchEntity
 
 @Database(
     entities = [RepoSearchEntity::class,
-        RepoEntity::class],
+        RepoEntity::class,
+        RemoteKeysEntity::class],
     version = 1,
     exportSchema = true,
 )
 @TypeConverters(
     ListStringConverter::class
 )
-abstract class GithubDatabase: RoomDatabase() {
+abstract class GithubDatabase : RoomDatabase() {
     abstract fun repoDao(): RepoDao
+
+    abstract fun remoteKeysDao(): RemoteKeysDao
 }
